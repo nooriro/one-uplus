801d7b132f29c4274cba9ffcd8493a38a522cd58
nooriro <noorirogit@gmail.com>
2022-01-13 17:00:00 +0900

    Add rebuild-cpp.cmd & sync-cpp.cmd

---
+++ rebuild-cpp.cmd
@@ -0,0 +1,2 @@
+@call "%USERPROFILE%\AndroidStudioProjects\OneUplus\app\src\main\cpp\clean"
+@call "%USERPROFILE%\AndroidStudioProjects\OneUplus\app\src\main\cpp\build"
---
+++ sync-cpp.cmd
@@ -0,0 +1,1 @@
+@robocopy "%USERPROFILE%\AndroidStudioProjects\OneUplus\app\src\main\cpp" "%~dp0OneUplus\app\src\main\cpp" /dcopy:dat /mir
