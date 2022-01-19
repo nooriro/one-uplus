09965448f817397ee30879939bce4b010f6a494b
nooriro <noorirogit@gmail.com>
2022-01-19 20:55:00 +0900

    Use multiple colors for highlighting escaped chars
    Escape state   Meaning                                  Background color
    ------------------------------------------------------------------------
    ES_NONE    0   non-escaped character                 no background color
    ES_TYPE1A  1   \a \b \t \n \v \f \r and \033          "Magenta" (870087)
    ES_TYPE1B  2   other 1-byte ascii escaped character      "Cyan" (008787)
    ES_TYPE1C  3   1-byte non-ascii escaped character          gray (5F5F5F)
    ES_TYPE2   4   multi(2)byte escaped character      light purple (AF87FF)
    ES_TYPE3   5   multi(3)byte escaped character        light pink (D787D7)
    ES_TYPE4   6   multi(4+)byte escaped character     light orange (D7875F)
    
    Screenshots
    -----------
    ![multicolor-ansi-escape-01-220119](https://user-images.githubusercontent.com/16160401/150124730-335b47fc-2c34-49d9-b8e3-d7b205eab4fa.png)
    ![multicolor-ansi-escape-02-220119](https://user-images.githubusercontent.com/16160401/150124753-0400e361-5d2b-4e6e-9840-795372aeb32f.png)
    ![multicolor-ansi-escape-03-220119](https://user-images.githubusercontent.com/16160401/150124785-e05a08b3-9f16-4bfe-93c3-af72981d6356.png)

--- OneUplus/app/src/main/cpp/jni/hello.c
+++ OneUplus/app/src/main/cpp/jni/hello.c
@@ -6,12 +6,36 @@
 #include <wchar.h>        // mbrtowc()
 //#include <limits.h>       // INT_MIN
 
-#define ANSI_START "\033[46m\033[48;5;30m"
-#define ANSI_END   "\033[0m"
+#define ES_NONE    0    // non-escaped character - print raw byte(s)
+#define ES_TYPE1A  1    // "\a" "\b" "\t" "\n" "\v" "\f" "\r" and "\033"
+#define ES_TYPE1B  2    // other 1-byte ascii escaped character ("\000" to "\177")
+#define ES_TYPE1C  3    // 1-byte non-ascii escaped character   ("\200" to "\377")
+#define ES_TYPE2   4    // multibyte (2-byte) escaped character ("\uhhhh")
+#define ES_TYPE3   5    // multibyte (3-byte) escaped character ("\uhhhh")
+#define ES_TYPE4   6    // multibyte (4-byte) escaped character ("\Uhhhhhhhh")
+
+// See: 256 (8-bit) color ANSI
+//      https://en.wikipedia.org/wiki/ANSI_escape_code#8-bit
+//      foreground color: ESC[38;5;NUMBERm
+//      background color: ESC[48;5;NUMBERm
+//      NUMBER in 16-231: 16 + 36*r + 6*g + b (0 <= r,g,b <= 5) : 216 colors (6x6x6 cube)
+// --------------------------------------------------------------------------------------
+//              +95           +40           +40           +40           +40
+//   0->0(0x00)    1->95(0x5f)   2->135(0x87)  3->175(0xaf)  4->215(0xd7)  5->255(0xff)
+//      0/51    +19   19/51    +8   27/51    +8   35/51    +8   43/51    +8   51/51
+//      0.000         0.372         0.529         0.686         0.843         1.000
+const char *const ANSI[] = {
+        "\033[0m",          // ES_NONE
+        "\033[48;5;90m",    // ES_TYPE1A : #870087 (L=31 a=58 b=-38) : "Magenta" (like "\033[45m", #800080)
+        "\033[48;5;30m",    // ES_TYPE1B : #008787 (L=51 a=-32 b=-9) : "Cyan"    (like "\033[46m", #008080)
+        "\033[48;5;59m",    // ES_TYPE1C : #5F5F5F (L=40 a=0 b=0)    : gray
+        "\033[48;5;141m",   // ES_TYPE2  : #AF87FF (L=64 a=35 b=-55) : light purple
+        "\033[48;5;176m",   // ES_TYPE3  : #D787D7 (L=67 a=40 b=-28) : light pink
+        "\033[48;5;173m"    // ES_TYPE4  : #D7875F (L=64 a=29 b=35)  : light orange
+};
 
 int intlen(int n);
-inline __attribute__((always_inline)) void in_esc(bool *state, bool ansi, FILE *fp);
-inline __attribute__((always_inline)) void out_esc(bool *state, bool ansi, FILE *fp);
+inline __attribute__((always_inline)) void update_es(int *es, int newes, bool ansi, FILE *fp);
 int printarg(char *arg, bool ansi, FILE *fp, int *cbesc);
 
 
@@ -51,31 +75,22 @@ int intlen(int n) {
     return 10; // INT_MAX == 2147483647 is ten-digit
 }
 
-inline __attribute__((always_inline))
-void in_esc(bool *state, bool ansi, FILE *fp) {
-    if (*state == false) {
-        if (ansi) { fputs(ANSI_START, fp); }
-        *state = true;
-    }
-}
-
-inline __attribute__((always_inline))
-void out_esc(bool *state, bool ansi, FILE *fp) {
-    if (*state == true) {
-        if (ansi) { fputs(ANSI_END, fp); }
-        *state = false;
-    }
-}
+// Call this function to update 'es'(escape state variable) before printing each character.
+// Do not update 'es' directly.
+inline __attribute__((always_inline)) //
+void update_es(int *es, int newes, bool ansi, FILE *fp) {
+    if (*es != newes) {
+        if (ansi) { fputs(ANSI[newes], fp); }
+        *es = newes;
+    } //
+} //
 
 int printarg(char *arg, bool ansi, FILE *fp, int *cbesc) {
-    if (cbesc) { *cbesc = 0; }  // count of escaped bytes
-    char *end = arg + strlen(arg);   // *end == '\0'
-    bool in = false;  //  Are we in the middle of escape sequences?
-    int ret = 0;      //  return value of mbrtowc()
-    mbstate_t mbstate = {{0}};  // state variable for mbrtowc()
-                                // should be initialized with 0
+    char *end = arg + strlen(arg);     // *end == '\0'
+    int es = ES_NONE;            // escape state
+    if (cbesc) { *cbesc = 0; }   // count of escaped bytes
+    mbstate_t mbstate = {{0}};   // state for mbrtowc()
     for (char c, *s = arg; (c = *s); s++) {
-        bool esc = false;  // Should char c be escaped?
         // "There are three character types, designated as char , signed char, and unsigned char."
         // "The three types char, signed char, and unsigned char are collectively called the character types."
         // "char is a separate type from the other two and is not compatible with either"
@@ -88,38 +103,38 @@ int printarg(char *arg, bool ansi, FILE *fp, int *cbesc) {
         // See: C comparison char and int
         //      https://stackoverflow.com/questions/1010330/c-comparison-char-and-int
         //
-        // ret : last return value of mbrtowc()
-        // ret == -2   means  "(Previous) s does not contain any complete multibyte charters until the end."
-        //     See: https://man7.org/linux/man-pages/man3/mbrtowc.3.html
-        //          https://en.cppreference.com/w/cpp/string/multibyte/mbrtowc
-        //          https://docs.microsoft.com/en-us/cpp/c-runtime-library/reference/mbrtowc?view=msvc-170
-        //          https://www.ibm.com/docs/en/i/7.1?topic=functions-mbrtowc-convert-multibyte-character-wide-character-restartable
-        // In this case, do not run mbrtowc() anymore and work with only the c value until the end.
-        if (ret != -2 && (signed char) c < 0) {   //  (signed char) c < 0  iff  (unsigned char) c >= 128
+        // See: mbrtowc() reference
+        //      https://man7.org/linux/man-pages/man3/mbrtowc.3.html
+        //      https://en.cppreference.com/w/cpp/string/multibyte/mbrtowc
+        //      https://docs.microsoft.com/en-us/cpp/c-runtime-library/reference/mbrtowc?view=msvc-170
+        //      https://www.ibm.com/docs/en/i/7.1?topic=functions-mbrtowc-convert-multibyte-character-wide-character-restartable
+        //
+        // See: UTF-8 encoding by code point
+        //      https://wiki.sei.cmu.edu/confluence/display/c/MSC10-C.+Character+encoding%3A+UTF8-related+issues
+        //      0000-007F:    1-byte  0xxxxxxx                             (7 bits of code point)
+        //      0080-07FF:    2-byte  110xxxxx 10xxxxxx                    (11 bits of code point)
+        //      0800-FFFF:    3-byte  1110xxxx 10xxxxxx 10xxxxxx           (16 bits of code point)
+        //      10000-1FFFFF: 4-byte  11110xxx 10xxxxxx 10xxxxxx 10xxxxxx  (21 bits of code point)
+        //
+        if ((unsigned char) c >= 128) {   //  equivalent to    (signed char) c < 0
             wchar_t wc;
-            ret = mbrtowc(&wc, s, end - s, &mbstate);
+            int ret = mbrtowc(&wc, s, end - s, &mbstate);
             if (ret < 0) {   // invalid multibyte sequence before next complete character (-1), or
                              // do not contain a complete multibyte character until the end (-2)
-                esc = true;
+                update_es(&es, ES_TYPE1C, ansi, fp);
             } else if ((wc >= 0x80 && wc < 0xa0)    // Unicode C1 controls (0080–009F)
                        || wc == 0x2028              // Line Separator      (2028)
                        || wc == 0x2029) {           // Paragraph Separator (2029)
                 // ---- Print multibyte character as escape sequence ---- //
-                in_esc(&in, ansi, fp);
-                // Print wc as "\uhhhh" or "\Uhhhhhhhh"
+                //               ("\uhhhh" or "\Uhhhhhhhh")               //
+                update_es(&es, ret == 2 ? ES_TYPE2 : ret == 3 ? ES_TYPE3 : ES_TYPE4, ansi, fp);
                 fprintf(fp, wc < 0x10000 ? "\\u%04x" : "\\U%08x", wc);
-                // 0000-007F:    1-byte  0xxxxxxx                             (7 bits of code point)
-                // 0080-07FF:    2-byte  110xxxxx 10xxxxxx                    (11 bits of code point)
-                // 0800-FFFF:    3-byte  1110xxxx 10xxxxxx 10xxxxxx           (16 bits of code point)
-                // 10000-1FFFFF: 4-byte  11110xxx 10xxxxxx 10xxxxxx 10xxxxxx  (21 bits of code point)
-                // See: https://wiki.sei.cmu.edu/confluence/display/c/MSC10-C.+Character+encoding%3A+UTF8-related+issues
-                int cb = wc < 0x80 ? 1 : wc < 0x800 ? 2 : wc < 0x10000 ? 3 : wc < 0x200000 ? 4 : wc < 0x4000000 ? 5 : 6;
-                s += cb - 1;
-                if (cbesc) { *cbesc += cb; }
+                s += ret - 1;
+                if (cbesc) { *cbesc += ret; }
                 continue;
             } else {
                 // ---- Print multibyte character as raw UTF-8 bytes ---- //
-                out_esc(&in, ansi, fp);
+                update_es(&es, ES_NONE, ansi, fp);
                 char *next = s + ret;
                 while (s < next) {
                     putc(*s++, fp);
@@ -127,31 +142,29 @@ int printarg(char *arg, bool ansi, FILE *fp, int *cbesc) {
                 s--;    // s == next - 1
                 continue;
             }
-        } else if ((signed char) c < 32 || c == 127) {  // (signed char) c < 32   iff
-                                                        // (unsigned char) c < 32 || (unsigned char) c >= 128
-            esc = true;
-        } // end of multibyte character printing
+        } else if (c < 32 || c == 127) {
+            update_es(&es, (c >= 7 && c < 14) || c == 27 ? ES_TYPE1A : ES_TYPE1B, ansi, fp);
+        } else {
+            update_es(&es, ES_NONE, ansi, fp);
+        }
         //
-        if (esc) {
+        if (es != ES_NONE) {
             // ---- Print single byte character as escape sequence ---- //
-            in_esc(&in, ansi, fp);
+            //      ("\a" "\b" "\t" "\n" "\v" "\f" "\r" or "\ooo")      //
             if (c >= 7 && c < 14) {
-                // Print c as "\a" "\b" "\t" "\n" "\v" "\f" "\r"
                 char *str = "abtnvfr";
                 putc('\\', fp);
                 putc(str[c - '\a'], fp);
             } else {
-                // Print c as "\ooo"
                 fprintf(fp, "\\%03o", (unsigned char) c);
             }
             if (cbesc) { (*cbesc)++; }
         } else {
             // ---- Print single byte character as raw byte ---- //
-            out_esc(&in, ansi, fp);
             putc(c, fp);
-        } // end of single byte character printing
+        }
         //
     } // end of for loop
-    out_esc(&in, ansi, fp);
+    update_es(&es, ES_NONE, ansi, fp);
     return end - arg;
 }
